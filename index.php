9e1f78011469ab14368606565a21528f3616a4ed
icaynia <icaynia@icaynia.com>
2016-12-28 17:36:26 +0900

    Facebook page link

--- index.php
+++ index.php
@@ -477,7 +477,7 @@
 						<div class="col-md-6 col-md-offset-3 text-center">
 							<p class="fh5co-social-icons">
 								<a href="#"><i class="icon-twitter2"></i></a>
-								<a href="#"><i class="icon-facebook2"></i></a>
+								<a href="https://www.facebook.com/bluesika/"><i class="icon-facebook2"></i></a>
 								<a href="#"><i class="icon-instagram"></i></a>
 								<a href="#"><i class="icon-dribbble2"></i></a>
 								<a href="https://www.youtube.com/channel/UCeKuEIpVAR3ZBHwf-BLPVkw"><i class="icon-youtube"></i></a>
